e29037eb64b2e9b98b625e356fcc00c912de4e42
francosebastian <francosebastian22@gmail.com>
2018-06-13 13:05:11 -0400

    Initial commit

---
+++ src/main/java/com/francosebastian/demographqlspring/DemoGraphqlSpringApplication.java
@@ -0,0 +1,12 @@
+package com.francosebastian.demographqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGraphqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGraphqlSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/francosebastian/demographqlspring/DemoGraphqlSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.francosebastian.demographqlspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoGraphqlSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
